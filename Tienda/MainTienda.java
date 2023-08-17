3f4b6d62e5b31f4096f1edbe5a3450d4a6c1334a
NickLimez <nicklimez93@gmail.com>
2023-08-17 17:32:07 -0300

    PrimerOrden
    Tienda, Colecciones y Relaciones

---
+++ Tienda/MainTienda.java
@@ -0,0 +1,13 @@
+package Colecciones.Tienda;
+
+import Colecciones.Menu.MenuTienda;
+
+public class MainTienda {
+
+    public static void main(String[] args) {
+
+        MenuTienda menuTienda = new MenuTienda();
+        menuTienda.menuTienda();
+
+    }
+}
